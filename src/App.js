99a92440b8d8c9ba280f7e395a5f03b8b4986876
taforyou <taforyou@hotmail.com>
2018-02-13 13:29:37 +0700

    Edit App.js -สร้าง constructor -สร้าง state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      counter : 99,
+    }
+
+  }
   render() {
     return (
       <div className="App">
         Hello World CMM341
+        <br/>
+        <h1>{this.state.counter}</h1>
         <button type="button">Click Me!</button>
       </div>
     );
