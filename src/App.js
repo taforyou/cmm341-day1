df57d05cf52695de26ff7cebef07e3fd5c12e6dd
taforyou <taforyou@hotmail.com>
2018-02-20 11:17:29 +0700

    Edit App.js Edit App.css
    เริ่มทำ Todo

--- src/App.js
+++ src/App.js
@@ -1,81 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { Layout, Form, Input, Icon, Row, Col, Button, Card, List} from 'antd';
+
+
 import './App.css';
-import { Input ,Button } from 'antd';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      counter : 0,
-      counterrrrr : 88,
-      inputText : '',
-      inputText1 : '',
-      inputText2 : '',
-      result : 0
-    }
-
-  }
-
-  handleOnAdd () {
-    console.log('handleOnAdd')
-    // ค่าเปลัี่ยนนะแต่ render() ไม่ทำงานมันจึงไม่ show ค่าใหม่
-    //this.state.counter++
-    //console.log(this.state.counter)
-    let temp = this.state.counter;
-    temp++;
-    
-    this.setState({
-      counter : temp
-    })
-
-    // อย่าทำเพราะเป็นการ Mutate state (จริงๆทำได้ แทนบรรทัดที่ 21>>26 เลย)
-    // this.setState({
-    //   counter : this.state.counter++
-    // })
-
-  }
-
-  handleOnCombine () {
-    let _temp1 = parseInt(this.state.inputText1)
-    let _temp2 = parseInt(this.state.inputText2)
-    let _result = _temp1 + _temp2
-    this.setState({ result : _result })
-  }
-
-  handleOnChange (_e) {
-    //console.log(_e.target.value)
-    this.setState({ inputText : _e.target.value })
-  }
-  handleOnChange1 (_e) {
-    this.setState({ inputText1 : _e.target.value })
-  }
-  handleOnChange2 (_e) {
-    //console.log(_e.target.value)
-    this.setState({ inputText2 : _e.target.value })
-  }
 
   render() {
+    const data = [
+        'text 1',
+        'text 2',
+        'text 3',
+      ];
+
+    const { Header, Footer, Sider, Content } = Layout;
+    const Search = Input.Search;
+    const FormItem = Form.Item;
+
     return (
-      <div className="App">
-        Hello World CMM341
-        <br/>
-        <h1>{this.state.counter}</h1>
-        <h2>{this.state.counterrrrr}</h2>
-        <Button type="primary" onClick={() => this.handleOnAdd()}>Click Me!</Button>
-        <br/>
-        <Input onChange={(e) => this.handleOnChange(e)} value={this.state.inputText} placeholder="Basic usage" defaultValue="ค่าเริ่มต้น"/>
-        <h1>{this.state.inputText}</h1>
-
-        <br/>
-
-        <Input onChange={(e) => this.handleOnChange1(e)} value={this.state.inputText1} defaultValue="ใส่เลขตัวแรก"/>
-        <Input onChange={(e) => this.handleOnChange2(e)} value={this.state.inputText2} defaultValue="ใส่เลขตัวที่สอง"/>
-        <Button type="primary" onClick={() => this.handleOnCombine()}>เอาเลขมาบวกกัน</Button>
-        <h1>{this.state.result}</h1>
-
-      </div>
+        <div className="App">
+            <Card style={{ width: 500 }}>
+                <h1>To-do-list</h1>
+                <div style={{
+                    marginBottom:'10px'
+                }}><Input addonAfter={
+                    <Button type="primary">Add</Button>
+                } /> </div>
+                <List
+                bordered
+                dataSource={data}
+                renderItem={item => (
+                    <List.Item actions={[<a><Icon type="close-circle" style={{ fontSize: 16, color: 'rgb(255, 145, 0)' }} /></a>]}>
+                        {item}
+                    </List.Item>
+                )}
+                />
+            </Card>
+        </div>
     );
   }
 }
