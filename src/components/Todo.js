fced29e35af21775031e6d1df8c539267e30c367
taforyou <taforyou@hotmail.com>
2018-02-23 09:42:06 +0700

    Edit App.js Edit Todo.js
    แก้ Todo.js , แก้ App.js ให้เหลือ components เดียว

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,44 @@
 import React, { Component } from "react";
+import { Layout, Form, Input, Icon, Row, Col, Button, Card, List } from "antd";
 
 export class Todo extends Component {
   render() {
+
     const _color = this.props.myColor
+    const data = ["text 1", "text 2", "text 3"];
+    const { Header, Footer, Sider, Content } = Layout;
+    const Search = Input.Search;
+    const FormItem = Form.Item;
+
     return (
-      <div>
-          <h1 style={{ color: _color }}>Hello New Component</h1>
-      </div>
+      <Card style={{ width: 500 }}>
+          <h1>To-do-list</h1>
+          <div
+            style={{
+              marginBottom: "10px"
+            }}
+          >
+            <Input addonAfter={<Button type="primary">Add</Button>} />{" "}
+          </div>
+          <List
+            bordered
+            dataSource={data}
+            renderItem={item => (
+              <List.Item
+                actions={[
+                  <a>
+                    <Icon
+                      type="close-circle"
+                      style={{ fontSize: 16, color: "rgb(255, 145, 0)" }}
+                    />
+                  </a>
+                ]}
+              >
+                {item}
+              </List.Item>
+            )}
+          />
+        </Card>
     );
   }
 }
